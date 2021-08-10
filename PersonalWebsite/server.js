7191440977e072824623fa44ce2b7b5cc33e9273
AlexRheault <alexdrhlt@gmail.com>
2021-08-09 19:03:28 -0700

    Changing to express routing

--- PersonalWebsite/server.js
+++ PersonalWebsite/server.js
@@ -1,8 +1,15 @@
 'use strict';
+const express = require("express");
+
 var http = require('http');
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\nTesting how the git stuff is going to work');
-}).listen(port);
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+
+const expressServer = app.listen(port, () => {
+    console.log("Server Started");
+})
