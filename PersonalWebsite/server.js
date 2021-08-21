4ee433baa372276f0ad2971b1004fe70e3564027
AlexRheault <alexdrhlt@gmail.com>
2021-08-21 12:55:25 -0700

    Adding support for emailing responses

--- PersonalWebsite/server.js
+++ PersonalWebsite/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
+const nodeMailer = require('nodemailer');
+require('dotenv').config();
 
-var http = require('http');
 var port = process.env.PORT || 1337;
 
 const app = express();
@@ -10,36 +11,60 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public/css")));
 
 app.use(express.urlencoded({
-	extend: false,
+    extend: false,
 }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	//	res.send("Run it");
-	res.sendFile(path.join(__dirname, "/public/index.html"));
+    //	res.send("Run it");
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 })
 
 app.get('/About', (req, res) => {
-	res.sendFile(path.join(__dirname, "public/about.html"));
+    res.sendFile(path.join(__dirname, "public/about.html"));
 })
 
 app.get('/Projects', (req, res) => {
-	res.sendFile(path.join(__dirname, "public/projects.html"));
+    res.sendFile(path.join(__dirname, "public/projects.html"));
 })
 
 app.get('/Resume', (req, res) => {
-	res.sendFile(path.join(__dirname, "public/resume.html"));
+    res.sendFile(path.join(__dirname, "public/resume.html"));
 })
 
 app.get('/Contact', (req, res) => {
-	res.sendFile(path.join(__dirname, "public/contact.html"));
+    res.sendFile(path.join(__dirname, "public/contact.html"));
 })
 
 app.post('/SubmitContact', (req, res) => {
-	console.log('Form Submission:', req.body);
-	res.json(req.body)
+    const data = req.body;
+
+    let Mailing = nodeMailer.createTransport("SMTP", {
+        service: "Gmail",
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASS
+        }
+    });
+
+    Mailing.sendMail({
+        from: data.Email,
+        to: process.env.USER,
+        subject: `${data.First}, ${data.Last} contact request`,
+        html: `${data.Pronouns}
+
+                ${data.Message}`
+    }, (err, res) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Contact Request Sent");
+        }
+
+        Mailing.close();
+    });
 });
 
 const expressServer = app.listen(port, () => {
-	console.log("Server Started");
+    console.log("Server Started");
 })
